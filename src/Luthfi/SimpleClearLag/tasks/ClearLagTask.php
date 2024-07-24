a5cd11a761a064ac51d51c5c38f55400a1e2dc0e
LuthMC <luthfi366822@gmail.com>
2024-07-24 21:32:13 +0700

    Add ClearLagTask.php

---
+++ src/Luthfi/SimpleClearLag/tasks/ClearLagTask.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Luthfi\SimpleClearLag\tasks;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use Luthfi\SimpleClearLag\Main;
+use pocketmine\world\World;
+
+class ClearLagTask extends Task {
+
+    /** @var Main */
+    private $plugin;
+    private $interval;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+        $this->interval = $this->plugin->getConfig()->get("interval", 5) * 60;
+    }
+
+    public function onRun(): void {
+        $warnings = [300, 60, 30, 5, 4, 3, 2, 1];
+        $currentTime = time() % $this->interval;
+
+        if (in_array($currentTime, $warnings)) {
+            $message = $this->plugin->getWarningMessage($this->interval - $currentTime);
+            foreach (Server::getInstance()->getOnlinePlayers() as $player) {
+                $player->sendActionBarMessage($message);
+            }
+        }
+
+        if ($currentTime === 0) {
+            foreach (Server::getInstance()->getWorldManager()->getWorlds() as $world) {
+                $this->clearEntities($world);
+            }
+            $message = $plugin->getClearMessage();
+            foreach (Server::getInstance()->getOnlinePlayers() as $player) {
+                $player->sendActionBarMessage($message);
+            }
+        }
+    }
+
+    private function clearEntities(World $world): void {
+        foreach ($world->getEntities() as $entity) {
+            if (!$entity instanceof Player) {
+                $entity->flagForDespawn();
+            }
+        }
+        foreach ($world->getDroppedItems() as $item) {
+            $item->flagForDespawn();
+        }
+    }
+}
